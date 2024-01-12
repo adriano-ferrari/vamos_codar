4a2e7e744ed6e3fcbe701f9dafdf83e8210656ad
adriano-ferrari <irarrefonairda@gmail.com>
2024-01-12 09:24:03 -0300

    Aula 5 - Templates HTML e Arquivos ESTÁTICOS

--- polls/views.py
+++ polls/views.py
@@ -5,7 +5,8 @@ from django.http import HttpResponse
 # Define uma view baseada em funcão.
 def index(request):
     # return HttpResponse('Olá Django - index')
-    return render(request, 'index.html')
+    # return render(request, 'index.html')
+    return render(request, 'index.html', {'titulo': 'Últimas Enquetes'})
 
 
 # define uma view baseada em funcão
