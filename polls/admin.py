643d6c23619466fcfa5073b5a401bbc8b7c0e66d
adriano-ferrari <irarrefonairda@gmail.com>
2024-01-19 08:08:20 -0300

    Aula 09 - O Poder da ÁREA ADMINISTRATIVA

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from polls.models import Question
+
 # Register your models here.
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'question_text', 'pub_date')
+    list_filter = ('pub_date',)
--- polls/models.py
+++ polls/models.py
@@ -4,3 +4,7 @@ from django.db import models
 class Question(models.Model):
     question_text = models.CharField('Pergunta', max_length=200)
     pub_date = models.DateField('Data de Publicacão')
+
+    class Meta:
+        verbose_name = 'Pergunta'
+        verbose_name_plural = 'Perguntas'
