d84b41913c02e78b1181fed5a5804ba3dbe41b19
adriano-ferrari <irarrefonairda@gmail.com>
2024-02-10 19:34:23 -0300

    [SEU PRIMEIRO App] COMO Criar um Aplicativo WEB [Django do Zero #03]

---
+++ webapp/polls/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+# Na linha abaixo importamos as "functions views".
+from .views import index, ola
+
+
+urlpatterns = [
+    path('index', index, name='index'),  # cria a rota /index
+    path('ola', ola, name='ola'),  # cria a rota /ola
+]
---
+++ webapp/polls/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+
+# Define uma 'function view' chamada index
+def index(request):
+    # return HttpResponse('Olá Django - index')
+    return render(request, 'index.html')
+
+
+# Define uma 'function view' chamada ola
+def ola(request):
+    return HttpResponse('Olá Django')
