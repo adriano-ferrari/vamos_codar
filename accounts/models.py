147a39f99c714b86433c81d4874dcde199aa94fb
adriano-ferrari <irarrefonairda@gmail.com>
2024-02-10 18:49:37 -0300

    Reiniciando o projeto

--- accounts/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import AbstractUser
-
-
-class CustomUser(AbstractUser):
-    data_nascimento = models.DateField('Data de Nascimento', null=True, blank=True)
-    cpf = models.CharField(max_length=11, null=True, blank=True)
-    imagem = models.FileField(upload_to='images/user', default=None, null=True, blank=True)
--- polls/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from polls.models import Question
-
-# Register your models here.
-@admin.register(Question)
-class QuestionAdmin(admin.ModelAdmin):
-    list_display = ('id', 'question_text', 'pub_date')
-    list_filter = ('pub_date',)
