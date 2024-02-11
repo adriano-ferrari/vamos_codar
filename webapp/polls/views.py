9965c33abd3ee59b6f920129a2f0c06370541a89
adriano-ferrari <irarrefonairda@gmail.com>
2024-02-11 10:03:35 -0300

    [MODELOS de DADOS e ORM] CRIE um Aplicativo WEB [Django do Zero #05]

--- webapp/polls/views.py
+++ webapp/polls/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 
+from .models import Question  # Acrescentar
+
 # Define uma 'function view' chamada index
 def index(request):
     # return HttpResponse('Olá Django - index')
@@ -10,5 +12,8 @@ def index(request):
     return render(request, 'index.html', {'titulo': 'Enquetes'})
 
 # Define uma 'function view' chamada ola
-def ola(request):
-    return HttpResponse('Olá Django')
+def ola(request):  # Modificar
+    # return HttpResponse('Olá django')
+    questions = Question.objects.all() # recupera todos questions do banco de dados
+    context = {'all_questions': questions } # cria um dicionário com todas as perguntas
+    return render(request, 'polls/questions.html', context) # renderiza o template e passa o contexto
