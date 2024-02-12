1c50e21e1cf275859450fe80fbe779f541c1e1aa
adriano-ferrari <irarrefonairda@gmail.com>
2024-02-12 14:46:02 -0300

    [COMO Relacionar Alternativas à Pergunta] CRIE um Aplicativo WEB [Django do Zero #15]

--- webapp/polls/models.py
+++ webapp/polls/models.py
@@ -11,3 +11,12 @@ class Question(models.Model):
         verbose_name = 'Pergunta'
         verbose_name_plural = 'Perguntas'
 
+
+class Choice(models.Model):
+    # chave estrangeira que vincula cada alternativa a uma pergunta
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200) # texto da alternativa
+    votes = models.IntegerField(default=0) # contagem de votos na alternativa
+
+    def __str__(self):
+        return self.choice_text
