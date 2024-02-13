dfc89cd9f39534d7dc9bef766bda0e5b463d8c65
adriano-ferrari <irarrefonairda@gmail.com>
2024-02-13 20:21:30 -0300

    [COMO Paginar Resultados] CRIE um Aplicativo WEB [Django do Zero #20]

--- webapp/polls/views.py
+++ webapp/polls/views.py
@@ -152,6 +152,8 @@ class QuestionListView(ListView):
     model = Question
     template_name = 'polls/question_list.html'
     context_object_name = 'questions'
+    paginate_by = 5  # quantidade de itens por página
+    ordering = ['-pub_date']  # ordenar pela data de publicaćão de forma inversa - decrescente
 
 
 class SobreTemplateView(TemplateView):
